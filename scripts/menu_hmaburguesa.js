8d78c4f106a62452fb4e0963ee74132bf7260467
100472369 <100472369@alumnos.uc3m.es>
2023-12-10 00:20:41 +0100

    Menú hamburguesa

---
+++ scripts/menu_hmaburguesa.js
@@ -0,0 +1,15 @@
+
+function myFunction() {
+    var x = $("#myLinks");
+    let logo = $("#logo_topnav");
+    logo.slideUp();
+    x.slideDown();
+
+}
+
+function showStripres(){
+    let x = $("#myLinks");
+    let logo = $("#logo_topnav");
+    logo.slideDown();
+    x.slideUp();
+}
