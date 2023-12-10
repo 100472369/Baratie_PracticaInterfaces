9d069bf001471aeda1e2974315e6b0babd15998e
100472369 <100472369@alumnos.uc3m.es>
2023-12-10 20:20:14 +0100

    Oh si mama mio

---
+++ scripts/migas_pan.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+  $("#pagina_principales").hide(); 
+  $("#pagina_postres").hide(); 
+  $("#pagina_cocteles").hide(); 
+
+
+  $("#Button_entrantes, #Boton_movimiento_back_entrantes").click(function(){
+    pasarEntrantes();
+  })
+
+  $("#Button_principales, #Boton_movimiento_to_principales, #Boton_movimiento_back_principales").click(function(){
+    pasarPrincipales();
+  })
+
+  $("#Button_postres, #Boton_movimiento_to_postres, #Boton_movimiento_back_postres").click(function(){
+    pasarPostres();
+  })
+
+  $("#Button_cocteles, #Boton_movimiento_to_cocteles").click(function(){
+    pasarCocteles();
+  })
+
+})
+
+
+function pasarEntrantes(){
+    $("#Button_postres").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_principales").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_cocteles").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_entrantes").css("background-color", "rgb(229,168,41)");
+    $("#pagina_entrantes").show(); 
+    $("#pagina_postres").hide(); 
+    $("#pagina_cocteles").hide(); 
+    $("#pagina_principales").hide(); 
+}
+
+
+function pasarPrincipales(){
+    $("#Button_entrantes").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_postres").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_cocteles").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_principales").css("background-color", "rgb(229,168,41)");
+    $("#pagina_entrantes").hide(); 
+    $("#pagina_postres").hide(); 
+    $("#pagina_cocteles").hide(); 
+    $("#pagina_principales").show(); 
+}
+
+function pasarPostres(){
+    $("#Button_entrantes").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_principales").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_cocteles").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_postres").css("background-color", "rgb(229,168,41)");
+    $("#pagina_entrantes").hide(); 
+    $("#pagina_postres").show(); 
+    $("#pagina_cocteles").hide(); 
+    $("#pagina_principales").hide(); 
+}
+
+function pasarCocteles(){
+    $("#Button_entrantes").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_principales").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_postres").css("background-color", "rgb(220, 220, 220)");
+    $("#Button_cocteles").css("background-color", "rgb(229,168,41)");
+    $("#pagina_entrantes").hide(); 
+    $("#pagina_postres").hide(); 
+    $("#pagina_cocteles").show(); 
+    $("#pagina_principales").hide(); 
+}
