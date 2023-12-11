4ca452da2d677ffe37d442e988b582597bf87961
ili <ilias.el@alumnos.uc3m.es>
2023-12-11 13:26:01 +0100

    Ya he adaptado lo del registro y redirecciones a principal o menu depende de si quieres registrarte normal o querer hacer ya el pedido

--- scripts/registro_principal.js
+++ scripts/registro_principal.js
@@ -4,10 +4,23 @@ $(document).ready(function () {
     const emailRegistrado = localStorage.getItem('email');
     const direccionRegistrada = localStorage.getItem('direccion');
     let linkToCompra = $("#to_pedido");
+    let botonExito = $("#boton_exito");
     if (!nombreRegistrado && !telefonoRegistrado && !emailRegistrado && !direccionRegistrada) {
-        linkToCompra.attr('href','registro.html')
+        linkToCompra.attr('href','registro.html');
+        botonExito.text('Continuar con el pedido');
+        botonExito.click(function () {
+            // Puedes agregar acciones adicionales al hacer clic en el botón de éxito
+            window.location.href = "pedido.html";
+        });
+
     }
+    /*
     else {
-        linkToCompra.attr('href', 'pedido.html')
-    }
+        linkToCompra.attr('href', 'pedido.html');
+        botonExito.text('Volver a la página principal');
+        botonExito.click(function () {
+            // Puedes agregar acciones adicionales al hacer clic en el botón de éxito
+            window.location.href = "index.html";
+        });
+    }*/
 });
