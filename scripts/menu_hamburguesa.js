5e158268dd585ce0a3f67c51fe570b1a429f3146
tomas <100461170@alumnos.uc3m.es>
2023-12-16 18:03:34 +0100

    creado index.js y comentado historia.css

--- scripts/menu_hamburguesa.js
+++ scripts/menu_hamburguesa.js
@@ -1,23 +1,3 @@
-$(document).ready(function () {
-    const header = $(".header_main_page")[0];
-    const toggleClass = "is-sticky";
-    $(window).scroll(function(){
-        const currentScroll = window.scrollY;
-        if (currentScroll > 150) {
-            header.classList.add(toggleClass);
-            $(".ul_link_page").hide();
-            $("#texto_baratie").hide();
-            $(".header_main_page").css("animation", ""); 
-          } else {
-            header.classList.remove(toggleClass);
-            $(".ul_link_page").show();
-            $("#texto_baratie").show();
-            
-            $(".header_main_page").css("animation", "slideUp 0.5s ease-out"); 
-          }  
-    });
-    
-    })
 
 function myFunction() {
     var x = $("#myLinks");
