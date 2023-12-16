1ef6e975d875785dc138e1d8d74f6b3488bc0e5a
100472369 <100472369@alumnos.uc3m.es>
2023-12-16 14:26:46 +0100

    yo queria a Michelangelo

--- scripts/menu_hamburguesa.js
+++ scripts/menu_hamburguesa.js
@@ -5,16 +5,15 @@ $(document).ready(function () {
         const currentScroll = window.scrollY;
         if (currentScroll > 150) {
             header.classList.add(toggleClass);
-            $(".links_page").hide();
+            $(".ul_link_page").hide();
             $("#texto_baratie").hide();
           } else {
             header.classList.remove(toggleClass);
-            $(".links_page").show();
+            $(".ul_link_page").show();
             $("#texto_baratie").show();
           }  
     });
-
-
+    
     })
 
 function myFunction() {
