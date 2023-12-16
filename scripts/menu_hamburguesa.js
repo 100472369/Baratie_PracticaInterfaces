2f3227eb4a747031d435ab5167639d043ed99251
tomas <100461170@alumnos.uc3m.es>
2023-12-16 14:59:16 +0100

    header dinamico

--- scripts/menu_hamburguesa.js
+++ scripts/menu_hamburguesa.js
@@ -7,10 +7,13 @@ $(document).ready(function () {
             header.classList.add(toggleClass);
             $(".ul_link_page").hide();
             $("#texto_baratie").hide();
+            $(".header_main_page").css("animation", ""); 
           } else {
             header.classList.remove(toggleClass);
             $(".ul_link_page").show();
             $("#texto_baratie").show();
+            
+            $(".header_main_page").css("animation", "slideUp 1s ease-out"); 
           }  
     });
     
