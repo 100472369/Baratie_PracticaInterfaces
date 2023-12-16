bc987f63254169d0d12123a58073ba3232c56a89
100472198 <100472198@alumnos.uc3m.es>
2023-12-16 18:37:50 +0100

    Cambios en redirecciones

--- scripts/menu_hamburguesa.js
+++ scripts/menu_hamburguesa.js
@@ -1,6 +1,20 @@
-
+$(document).ready(function() {
+    let idioma = $('html').attr('lang');
+    const toCompra = $("#pedidos_desde_hamburguesa");
+    const nombreRegistrado = localStorage.getItem('nombre');
+    const telefonoRegistrado = localStorage.getItem('telefono');
+    const emailRegistrado = localStorage.getItem('email');
+    const direccionRegistrada = localStorage.getItem('direccion');
+    toCompra.click(function() {
+        // Si venimos desde el menu de compra y aun no disponemos de cookies, vamos a redirigir a la pagina de registro
+        if (!(nombreRegistrado && telefonoRegistrado && emailRegistrado && direccionRegistrada)) {
+            if (idioma === "es") toCompra.attr('href','registro.html?source=pagina_pedido');
+            else toCompra.attr('href','registro_eng.html?source=pagina_pedido');
+        }
+    });
+});
 function myFunction() {
-    var x = $("#myLinks");
+    let x = $("#myLinks");
     let logo = $("#logo_topnav");
     logo.slideUp();
     x.slideDown();
--- scripts/registro_principal.js
+++ scripts/registro_principal.js
@@ -1,14 +1,23 @@
 $(document).ready(function () {
+    let idioma = $('html').attr('lang');
     const nombreRegistrado = localStorage.getItem('nombre');
     const telefonoRegistrado = localStorage.getItem('telefono');
     const emailRegistrado = localStorage.getItem('email');
     const direccionRegistrada = localStorage.getItem('direccion');
     let toCompra = $("#to_pedido");
+    let botonPedido = $("#boton_hacer_pedido");
     // Si venimos desde el menu de compra y aun no disponemos de cookies, vamos a redirigir a la pagina de registro
     toCompra.click(function() {
         if (!(nombreRegistrado && telefonoRegistrado && emailRegistrado && direccionRegistrada)) {
             // Ademas, se incluye una fuente especial para poder manejar este evento en especifico
-            toCompra.attr('href','registro.html?source=pagina_pedido');
+            if (idioma === "es") toCompra.attr('href','registro.html?source=pagina_pedido');
+            else toCompra.attr('href','registro_eng.html?source=pagina_pedido');
+        }
+    });
+    botonPedido.click(function() {
+        if (!(nombreRegistrado && telefonoRegistrado && emailRegistrado && direccionRegistrada)) {
+            if (idioma === "es") botonPedido.attr('href','registro.html?source=pagina_pedido');
+            else botonPedido.attr('href','registro_eng.html?source=pagina_pedido');
         }
     });
 });
